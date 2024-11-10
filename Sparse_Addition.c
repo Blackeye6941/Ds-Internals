cd462aadbc1fec315d4543fd82d8c6d16d2ab84e
Blackeye6941 <leok37316@gmail.com>
2024-11-10 18:26:07 +0530

    Update hotel management

---
+++ Sparse_Addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define MAX_SIZE 20
+struct Sparse{
+    int row;
+    int col;
+    int val;
+};
+
+int main(){
+    struct Sparse S1[MAX_SIZE], S2[MAX_SIZE], S3[2*MAX_SIZE];
+}
